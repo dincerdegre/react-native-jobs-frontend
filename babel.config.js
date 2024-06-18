c205b3b5eab1e1cb281ce5effd2f23edbf71c104
Dincer Degre <info@dincerdegre.com>
2024-06-18 21:01:20 +0300

    added dotenv plugin

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(false);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel"],
+    plugins: [["module:react-native-dotenv"]],
   };
 };
